ffdaaf27f23fc8002af5a81a5fc357a60ed88595
Mario Martinez <amakenadog@gmail.com>
2016-05-24 11:19:49 -0600

    Improved start and ending time for event logic

--- grails-app/assets/javascripts/start-ending-time.js
+++ grails-app/assets/javascripts/start-ending-time.js
@@ -1,15 +1,33 @@
-$('#startTime').on('change', function() {
-    var opt = $("option:selected", this);
-    var next = opt.next().text();
-    var endingTime = $('#endingTime');
+$(function() {
+    $('#startTime').on('change', function() {
+        setTime()
+    });
 
-    endingTime.find('option').each(function() {
-        var $this = $(this);
+    setTime();
 
-        if ($this.text() == next) {
-            $this.attr('selected', 'selected');
-        } else {
-            $this.removeAttr('selected');
+    function setTime() {
+        var offset = parseInt($('#startTime').find('option:selected').val(), 10),
+            endingTime = $('#endingTime'),
+            range = [
+                {time: 9, value: "9:00"},
+                {time: 10, value: "10:00"},
+                {time: 11, value: "11:00"},
+                {time: 12, value: "12:00"},
+                {time: 13, value: "1:00"},
+                {time: 14, value: "2:00"},
+                {time: 15, value: "3:00"},
+                {time: 16, value: "4:00"},
+                {time: 17, value: "5:00"}
+            ];
+
+        endingTime.find('option').remove();
+
+        for (var i = 0; i < range.length; i++) {
+            if (range[i].time > offset) {
+                var option = $('<option>', { value: range[i].time, text: range[i].value });
+
+                endingTime.append(option);
+            }
         }
-    })
+    }
 });
