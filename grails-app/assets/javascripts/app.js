1fd4245796ca3e9136c15d49fa8335fd9f8067e0
Mario Martinez <amakenadog@gmail.com>
2017-04-03 10:18:36 -0600

    Remove no needed files

--- grails-app/assets/javascripts/app.js
+++
@@ -1,8 +0,0 @@
-//= require /jquery/dist/jquery.min.js
-//= require /bootstrap/dist/js/bootstrap.min.js
-
-(function() {
-    $('.macguiver').on('click', function() {
-        console.log($(this));
-    });
-})();
